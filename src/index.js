d65e4ab04ed1cc8df38a61f5d3886d8cf6dbfbe4
Archit Gupta <arch1995@gmail.com>
2018-06-07 16:57:22 +0530

    Update without expo config

--- src/index.js
+++ src/index.js
@@ -1,151 +1,149 @@
-import React, {Component} from 'react';
-import PropTypes from 'prop-types'
-import {View, StyleSheet, Animated} from 'react-native';
+import React, { Component } from 'react'
+import { string, number, element, oneOfType, arrayOf, bool } from 'prop-types'
+import { View, Animated } from 'react-native'
 
-import Expo, { Svg } from 'expo';
-const AnimatedSvg = Animated.createAnimatedComponent(Svg);
-const {interpolate} = require('d3-interpolate');
+import Svg, {
+  G,
+  LinearGradient,
+  Rect,
+  ClipPath,
+  Defs,
+  Stop,
+} from 'react-native-svg'
 
-export default class SvgAnimatedLinearGradient extends Component {
-    constructor(props) {
-        super(props);
+const AnimatedSvg = Animated.createAnimatedComponent(Svg)
+const { interpolate } = require('d3-interpolate')
 
-        this.state = {
-            offsetValues: [
-                '-2', '-1.5', '-1'
-            ],
-            offsets: [
-                '0.0001', '0.0002', '0.0003' // Avoid duplicate value cause error in Android
-            ],
-            frequence: props.duration / 2
-        }
-        this._isMounted = false;
-        this._animate = new Animated.Value(0)
-        this.loopAnimation = this
-            .loopAnimation
-            .bind(this)
+export default class ContentLoader extends Component {
+  static offsetValueBound = (x) => {
+    if (x > 1) {
+      return '1'
     }
-    offsetValueBound(x) {
-        if (x > 1) {
-            return '1'
-        }
-        if (x < 0) {
-            return '0'
-        }
-        return x
+    if (x < 0) {
+      return '0'
     }
-    componentDidMount(props) {
-        this._isMounted = true
-        this.loopAnimation()
+    return x
+  }
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      offsetValues: [
+        '-2', '-1.5', '-1',
+      ],
+      offsets: [
+        '0.0001', '0.0002', '0.0003', // Avoid duplicate value cause error in Android
+      ],
     }
-    componentWillUnmount() {
-        this._isMounted = false
+    this.animate = new Animated.Value(0)
+    this.animate.addListener(this.animationListener)
+    this.loopAnimation = Animated.loop(
+      Animated.sequence([
+        Animated.timing(this.animate, {
+          toValue: 1,
+          duration: this.props.duration,
+        }),
+        Animated.timing(this.animate, {
+          toValue: 0,
+          duration: 0,
+        }),
+      ]))
+  }
+
+  componentDidMount() {
+    this.loopAnimation.start()
+  }
+
+  componentWillUnmount() {
+    this.animate.removeAllListeners()
+    this.loopAnimation.stop()
+  }
+
+  animationListener = (anim) => {
+    const interpolator = interpolate(this.state, {
+      offsetValues: ['1.2', '1.7', '2.2'],
+    })
+    const newState = interpolator(anim.value)
+    const offsetValues = []
+    offsetValues[0] = ContentLoader.offsetValueBound(newState.offsetValues[0])
+    offsetValues[1] = ContentLoader.offsetValueBound(newState.offsetValues[1])
+    offsetValues[2] = ContentLoader.offsetValueBound(newState.offsetValues[2])
+
+    if (offsetValues[0] !== offsetValues[1] ||
+      offsetValues[0] !== offsetValues[2] ||
+      offsetValues[1] !== offsetValues[2]) {
+      this.setState({ offsets: offsetValues })
     }
-    loopAnimation() {
-        if (!this._isMounted) return;
-        // setup interpolate
-        let interpolator = interpolate(this.state, {
-            offsetValues: ['1', '1.5', '2']
-        });
+  }
 
-        // start animation
-        let start = Date.now();
-        this._animation = () => {
-            const now = Date.now();
-            let t = (now - start) / this.props.duration;
-            if (t > 1) {
-                t = 1
-            }
+  render() {
+    return (
+      <View>
+        <AnimatedSvg height={this.props.height} width={this.props.width}>
+          <Defs>
+            <LinearGradient id="grad" x1={this.props.x1} y1={this.props.y1} x2={this.props.x2} y2={this.props.y2}>
+              <Stop
+                offset={this.state.offsets[0]}
+                stopColor={this.props.primaryColor}
+                stopOpacity="1"
+              />
+              <Stop
+                offset={this.state.offsets[1]}
+                stopColor={this.props.secondaryColor}
+                stopOpacity="1"
+              />
+              <Stop
+                offset={this.state.offsets[2]}
+                stopColor={this.props.primaryColor}
+                stopOpacity="1"
+              />
+            </LinearGradient>
+            <ClipPath id="clip">
+              <G>
+                {this.props.children}
+              </G>
+            </ClipPath>
+          </Defs>
 
-            let newState = interpolator(t);
-            let offsetValues = [];
-            offsetValues[0] = this.offsetValueBound(newState.offsetValues[0]);
-            offsetValues[1] = this.offsetValueBound(newState.offsetValues[1]);
-            offsetValues[2] = this.offsetValueBound(newState.offsetValues[2]);
-            
-            // Make sure at least two offsets is different
-            if (offsetValues[0] !== offsetValues[1] || offsetValues[0] !==  offsetValues[2] || offsetValues[1] !== offsetValues[2]) {
-                this._isMounted && this.setState({offsets: offsetValues});
-            }
-            if (t < 1) {
-                requestAnimationFrame(this._animation);
-            }
-        }
-        requestAnimationFrame(this._animation);
-
-        // Setup loop animation
-        Animated.sequence([
-            Animated.timing(this._animate, {
-                toValue: 1,
-                duration: this.state.frequence
-            }),
-            Animated.timing(this._animate, {
-                toValue: 0,
-                duration: this.state.frequence
-            })
-        ]).start((event) => {
-            if (event.finished) {
-                this.loopAnimation()
-            }
-        })
-    }
-    render() {
-
-        return (
-            <AnimatedSvg height={this.props.height} width={this.props.width}>
-                <Svg.Defs>
-                    <Svg.LinearGradient id="grad" x1={this.props.x1} y1={this.props.y1} x2={this.props.x2} y2={this.props.y2}>
-                        <Svg.Stop
-                            offset={this.state.offsets[0]}
-                            stopColor={this.props.primaryColor}
-                            stopOpacity="1"/>
-                        <Svg.Stop
-                            offset={this.state.offsets[1]}
-                            stopColor={this.props.secondaryColor}
-                            stopOpacity="1"/>
-                        <Svg.Stop
-                            offset={this.state.offsets[2]}
-                            stopColor={this.props.primaryColor}
-                            stopOpacity="1"/>
-                    </Svg.LinearGradient>
-                    <Svg.ClipPath id="clip">
-                        <Svg.G>
-                            {this.props.children}
-                        </Svg.G>
-                    </Svg.ClipPath>
-                </Svg.Defs>
-
-                <Svg.Rect
-                    x="0"
-                    y="0"
-                    height={this.props.height}
-                    width={this.props.width}
-                    fill="url(#grad)"
-                    clipPath="url(#clip)"/>
-            </AnimatedSvg>
-        );
-    }
+          <Rect
+            x="0"
+            y="0"
+            height={this.props.height}
+            width={this.props.width}
+            fill="url(#grad)"
+            clipPath="url(#clip)"
+          />
+        </AnimatedSvg>
+      </View>
+    )
+  }
 }
-SvgAnimatedLinearGradient.propTypes = {
-    primaryColor: PropTypes.string,
-    secondaryColor: PropTypes.string,
-    duration: PropTypes.number,
-    width: PropTypes.number,
-    height: PropTypes.number,
-    x1: PropTypes.string,
-    y1: PropTypes.string,
-    x2: PropTypes.string,
-    y2: PropTypes.string,
+ContentLoader.propTypes = {
+  children: oneOfType([
+    element.isRequired,
+    arrayOf(oneOfType([
+      element,
+      bool,
+    ]))],
+  ).isRequired,
+  primaryColor: string,
+  secondaryColor: string,
+  duration: number,
+  width: number,
+  height: number,
+  x1: string,
+  y1: string,
+  x2: string,
+  y2: string,
 }
-SvgAnimatedLinearGradient.defaultProps = {
-    primaryColor: '#eeeeee',
-    secondaryColor: '#dddddd',
-    duration: 2000,
-    width: 300,
-    height: 200,
-    x1: '0',
-    y1: '0',
-    x2: '100%',
-    y2: '0'
-
+ContentLoader.defaultProps = {
+  primaryColor: '#eeeeee',
+  secondaryColor: '#dddddd',
+  duration: 2000,
+  width: 300,
+  height: 200,
+  x1: '0',
+  y1: '0',
+  x2: '100%',
+  y2: '0',
 }
